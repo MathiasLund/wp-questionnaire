69a5f980b977fca8c8bd3741b44b11be9db29576
Mathias Lund <jenslund@jenss-imac-2.home>
2018-11-25 17:22:03 +0100

    added config.php file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+  function site_url() {
+    return 'http://localhost:9999/projects/wp-questionnaire';
+  }
+
+  function trainer_url() {
+    return 'https://app.zenfitapp.com/trainer/leadCreateQuery/163/en';
+  }
+
+  function get_header() {
+    return '<p></p>';
+  }
+
+?>
